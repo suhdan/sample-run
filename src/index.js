64dbfd500d28c578def6100e5caf64a5341ae41e
Daniel Suh <dansuh3593@gmail.com>
2020-03-28 14:23:00 -0400

    Setup webpack dev server and transpiling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log("hi");
+document.addEventListener("DOMContentLoaded", () => {});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
   }
 };
