59f2f88a50c9795c03079f7dc2eb819ed0914359
Jaehoon Han <JaehoonHan1@gmail.com>
2021-01-27 15:33:31 -0500

    Add start screen

--- src/index.js
+++ src/index.js
@@ -8,76 +8,94 @@ import buildingsImg from "./img/buildings.png"
 import sidewalkImg from "./img/sidewalk.png"
 import playerSprite from "./img/playersprite.png"
 import Background from "./components/background.js"
-import gameover from "./components/gameover.js"
+import gameOver from "./components/gameover.js"
 
 document.addEventListener("DOMContentLoaded", () => {
 
     let scoreText;
+    let highscoreText;
     let levelText;
+    let startText;
     let player;
     let buildings;
     let sidewalk;
 
-    function Start() {
-        player = new Player(playerSprite, 25, canvas.height - 150, 40, 112);
+    function initialize() {
+        player = new Player(playerSprite, 25, canvas.height - 250, 40, 112);
         buildings = [
             new Background(buildingsImg, 0, -15, canvas.width, canvas.height, 3),
             new Background(buildingsImg, canvas.width, -15, canvas.width, canvas.height, 3)
         ]
         sidewalk = [
-            new Background(sidewalkImg, 0, canvas.height - 30, 1000, 30, game.speed),
+            new Background(sidewalkImg, -100, canvas.height - 30, 1000, 30, game.speed),
             new Background(sidewalkImg, canvas.width, canvas.height-30, 1000, 30, game.speed)
         ]
         if (localStorage.getItem('highScore')) {
             game.highScore = localStorage.getItem('highScore');
         }
-        scoreText = new Text("Score: " + game.score + " HighScore: " + game.highScore, canvas.width - 35, 40, "right", '#FFFFFF', 35);
-        levelText = new Text("Level " + game.level, canvas.width/2, canvas.height/2, "center", 'yellow', 45);
-        
-        requestAnimationFrame(Update);
+        scoreText = new Text("Score: " + game.score, canvas.width - 250, 40, "left", '#FFFFFF', 35);
+        highscoreText = new Text("HighScore: " + game.highScore, canvas.width - 335, 80, "left", '#FFFFFF', 35);
+        levelText = new Text("Level " + game.level, canvas.width / 2, canvas.height / 2, "center", 'yellow', 45);
+        startText = new Text("Hit the 'Space Bar' or 'Arrow Up' key to play.", canvas.width / 2, canvas.height / 2, "center", 'white', 25);       
     }
 
-    function Update () {
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        buildings[0].scroll();
-        buildings[1].scroll();
-        sidewalk[0].acceleratedScroll();
-        sidewalk[1].acceleratedScroll();
-        scoreText.draw();
-        scoreText.text = "Score: " + game.score + " HighScore: " + game.highScore;
-        
-        game.frame++;
-        if(game.frame % 10 == 0) {
-            game.score++;
+    function startScreen() {
+        if (game.state == 'playing') {
+            requestAnimationFrame(update);
         }
-        for (let i = 0; i < game.obstacles.length; i++){
-            game.obstacles[i].animate();
-            if(checkCollision(player, game.obstacles[i])){
-                player.xFrame = 4;
-                player.draw();
-                for (let j = i+1; j < game.obstacles.length; j++)
-                    game.obstacles[j].animate();
-                gameover();
-                return; 
+        else {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            buildings[0].draw();
+            sidewalk[0].draw();
+            startText.draw();
+            player.startScreen();
+            requestAnimationFrame(startScreen);
+        }
+    }
+
+    function update () {
+
+        if (game.state == 'playing') {
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            buildings[0].scroll();
+            buildings[1].scroll();
+            sidewalk[0].acceleratedScroll();
+            sidewalk[1].acceleratedScroll();
+            scoreText.draw();
+            highscoreText.draw();
+            scoreText.text = "Score: " + game.score;
+            highscoreText.update("HighScore: " + game.highScore);
+            player.animate();
+            spawnObstacle();
+            for (let i = 0; i < game.obstacles.length; i++) {
+                game.obstacles[i].animate();
+                checkCollision(player, game.obstacles[i])
+            }
+            game.frame++;
+            if (game.frame % 10 == 0) {
+                game.score++;
+            }
+            if (game.score > game.highScore) {
+                game.highScore = game.score;
+            }
+            //give player a 80 frame break, 50 frames before each level increase
+            if (game.frame % (game.levelIncr * game.level - 50) == 0) {
+                game.spawnTimer = 80;
+            }
+            if (game.frame % 500 == 0 && game.speed <= game.speedMax) {
+                game.speed += game.speedInc;
+                game.level++;
+                //game.spawnTimer = 80;
+                //play game increase sound
             }
         }
-        spawnObstacle(); 
-        player.animate();
-        if (game.score > game.highScore) {
-            game.highScore = game.score;
-        }
-        if(game.frame % (game.levelInc * game.level -50) == 0) {
-            game.spawnTimer = 80;
-        } 
-        if (game.frame % 500 == 0 && game.speed <= game.speedMax) {
-            game.speed += game.speedInc;
-            game.level++;
-            //game.spawnTimer = 80;
-            //play game increase sound
-            }    
-        requestAnimationFrame(Update);
+        if (game.state == 'over') {
+            gameOver(buildings, sidewalk, player, scoreText, highscoreText);
+        }  
+        requestAnimationFrame(update);
     }
-
-    Start();
+    
+    initialize();
+    startScreen();
 });
