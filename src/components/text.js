76e18dc06446138f12bd8632759e5358f02ffd35
Jaehoon Han <JaehoonHan1@gmail.com>
2021-01-27 15:29:50 -0500

    Add text update function and fontfamily attr

--- src/components/text.js
+++ src/components/text.js
@@ -1,18 +1,23 @@
 import { canvas, ctx } from './config.js' 
 
 export default class Text {
-    constructor (text, xPos, yPos, alignment, color, size){
+    constructor (text, xPos, yPos, alignment, color, size, fontFamily){
         this.text = text;
         this.xPos = xPos;
         this.yPos = yPos;
         this.alignment = alignment;
         this.color = color;
         this.size = size;
+        this.fontFamily = 'Orbitron';
     }
+    update(newText) {
+        this.text = newText;
+    }
+
     draw () {
         ctx.beginPath();
         ctx.fillStyle = this.color;
-        ctx.font = this.size + "px monospace"; 
+        ctx.font = this.size + "px " + this.fontFamily; 
         ctx.textAlign = this.alignment;
         ctx.fillText(this.text, this.xPos, this.yPos);
         ctx.closePath();
